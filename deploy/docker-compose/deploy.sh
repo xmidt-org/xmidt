ca6171a87f9076f4bd440f55191821f071c32f6a
Jack Murdock <kcajmagic@gmail.com>
2020-05-08 14:31:44 -0700

    adding argus to docker-compose

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -8,15 +8,9 @@ echo "Building Simulator..."
 docker build -t simulator:local $ROOT_DIR/simulator
 popd
 
-echo "Building goaws..."
-git clone git@github.com:kcajmagic/goaws.git /tmp/goaws
-pushd /tmp/goaws
-git checkout adding_http_support
-docker build -t goaws:local .
-popd
-
 echo "Running services..."
 CADUCEUS_VERSION=${CADUCEUS_VERSION:-0.2.7} \
+ARGUS_VERSION=${ARGUS_VERSION:-0.1.1} \
 TR1D1UM_VERSION=${TR1D1UM_VERSION:-0.5.0} \
 SCYTALE_VERSION=${SCYTALE_VERSION:-0.1.5} \
 PETASOS_VERSION=${PETASOS_VERSION:-0.1.4} \
@@ -24,4 +18,3 @@ TALARIA_VERSION=${TALARIA_VERSION:-0.5.1} \
 THEMIS_VERSION=${THEMIS_VERSION:-0.4.3} \
 SIMULATOR_VERSION=${SIMULATOR_VERSION:-local} \
 docker-compose -f $ROOT_DIR/deploy/docker-compose/docker-compose.yml up -d $@
-
