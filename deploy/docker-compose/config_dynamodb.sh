96e7b4764f7b7783272e84dcc631ac0ea136073d
Joel Unzain <joe94@users.noreply.github.com>
2020-12-18 13:38:13 -0800

    Replacing SNS with Argus (#38)
    * use argus for tr1d1um as webhook store
    
    * update config that includes admin token
    
    * save changes
    
    * small bash file linting fixes
    
    * consul latest works
    
    * Remove SNS as a dep

---
+++ deploy/docker-compose/config_dynamodb.sh
@@ -0,0 +1,15 @@
+AWS_ACCESS_KEY_ID=accessKey AWS_SECRET_ACCESS_KEY=secretKey aws dynamodb  --endpoint-url http://localhost:8000 create-table \
+    --table-name gifnoc \
+    --attribute-definitions \
+        AttributeName=bucket,AttributeType=S \
+        AttributeName=uuid,AttributeType=S \
+    --key-schema \
+        AttributeName=bucket,KeyType=HASH \
+        AttributeName=uuid,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5 \
+    --stream-specification StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES \
+    --region us-east-2
+
+
+AWS_ACCESS_KEY_ID=accessKey AWS_SECRET_ACCESS_KEY=secretKey aws dynamodb  --endpoint-url http://localhost:8000 --region us-east-2 update-time-to-live --table-name gifnoc --time-to-live-specification "Enabled=true, AttributeName=expires"
--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-DIR=$( cd $(dirname $0) ; pwd -P )
+DIR=$( cd "$(dirname "$0")" || exit; pwd -P )
 ROOT_DIR=$DIR/../../
 
-pushd $ROOT_DIR
+pushd "$ROOT_DIR" || exit
 echo "Building Simulator..."
-docker build -t simulator:local $ROOT_DIR/simulator
-popd
+docker build -t simulator:local "$ROOT_DIR/simulator"
+popd || exit
 
 echo "Building goaws..."
 git clone git@github.com:kcajmagic/goaws.git /tmp/goaws
-pushd /tmp/goaws
+pushd /tmp/goaws || exit
 git checkout adding_http_support
 docker build -t goaws:local .
-popd
+popd || exit
 
 echo "Running services..."
-CADUCEUS_VERSION=${CADUCEUS_VERSION:-0.2.7} \
-TR1D1UM_VERSION=${TR1D1UM_VERSION:-0.5.0} \
+CADUCEUS_VERSION=${CADUCEUS_VERSION:-0.4.2} \
+ARGUS_VERSION=${ARGUS_VERSION:-0.3.9} \
+TR1D1UM_VERSION=${TR1D1UM_VERSION:-0.5.3} \
 SCYTALE_VERSION=${SCYTALE_VERSION:-0.1.5} \
 PETASOS_VERSION=${PETASOS_VERSION:-0.1.4} \
 TALARIA_VERSION=${TALARIA_VERSION:-0.5.9} \
 THEMIS_VERSION=${THEMIS_VERSION:-0.4.3} \
 SIMULATOR_VERSION=${SIMULATOR_VERSION:-local} \
-docker-compose -f $ROOT_DIR/deploy/docker-compose/docker-compose.yml up -d $@
+docker-compose -f "$ROOT_DIR/deploy/docker-compose/docker-compose.yml" up -d "$@"
+
+bash config_dynamodb.sh
 
