3b5f4d350f05599e3ec39ed50601ac366a34eb5e
mpicci200_comcast <piccione99@yahoo.com>
2025-01-21 12:09:40 -0500

    remove commented code

--- deploy/docker-compose-new/deploy.sh
+++ deploy/docker-compose-new/deploy.sh
@@ -24,30 +24,3 @@ docker compose -f $ROOT_DIR/deploy/docker-compose-new/docker-compose.yml up -d $
 if [[ $? -ne 0 ]]; then
   exit 1
 fi
-
-# sleep 10
-# AWS_ACCESS_KEY_ID=accessKey AWS_SECRET_ACCESS_KEY=secretKey aws dynamodb  --endpoint-url http://localhost:8000 describe-table --table-name gifnoc --region us-east-2 --output text > /dev/null 2> /dev/null
-# if [[ $? -ne 0 ]]; then
-#   AWS_ACCESS_KEY_ID=accessKey AWS_SECRET_ACCESS_KEY=secretKey aws dynamodb  --endpoint-url http://localhost:8000 create-table \
-#       --table-name gifnoc \
-#       --attribute-definitions \
-#           AttributeName=bucket,AttributeType=S \
-#           AttributeName=expires,AttributeType=N \
-#           AttributeName=id,AttributeType=S \
-#       --key-schema \
-#           AttributeName=bucket,KeyType=HASH \
-#           AttributeName=id,KeyType=RANGE \
-#     --global-secondary-indexes \
-#         "[{\"IndexName\": \"Expires-index\",\"KeySchema\":[{\"AttributeName\":\"bucket\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"expires\",\"KeyType\":\"RANGE\"}], \
-#         \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}]" \
-#       --provisioned-throughput \
-#           ReadCapacityUnits=10,WriteCapacityUnits=5 \
-#       --stream-specification StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES \
-#       --region us-east-2 \
-#       --output text
-
-#   AWS_ACCESS_KEY_ID=accessKey AWS_SECRET_ACCESS_KEY=secretKey aws dynamodb \
-#     --endpoint-url http://localhost:8000 --region us-east-2 update-time-to-live \
-#     --table-name gifnoc --time-to-live-specification "Enabled=true, AttributeName=expires" \
-#     --output text
-# fi
